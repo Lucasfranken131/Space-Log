9111d6aa280947f6ff8998ea5bdf92c7b77fed8a
Lucas Franco <105955764+Lucasfranken131@users.noreply.github.com>
2022-10-21 15:42:57 -0300

    Update protect_cliente.php

--- protect_cliente.php
+++ protect_cliente.php
@@ -2,7 +2,7 @@
     if(!isset($_SESSION)){
         session_start();
     }
-	if(!isset($_SESSION['id'])){
+	if(!isset($_SESSION['id_cliente'])){
 		die("Você não está logado. <p><a href=\"login.html\">Entrar</a></p>");
 	}
 ?>
