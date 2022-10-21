ce778ebd32a068ed0687030d71793774a09a83a0
Lucasfranken131 <105955764+Lucasfranken131@users.noreply.github.com>
2022-10-20 21:23:57 -0300

    Update indo2.php
    Troquei as coisas aqui sobre o email e senha.

--- indo2.php
+++ indo2.php
@@ -1,26 +1,26 @@
 <?php
     include("conexao.php");
 	
-	if(isset($_POST['cpf']) || isset($_POST['senha'])){
+	if(isset($_POST['email']) || isset($_POST['senha'])){
 		
-		if (strlen($_POST['cpf']) == 0){
-			echo "Preencha o campo de CPF"."<br>";
+		if (strlen($_POST['email']) == 0){
+			echo "Preencha o campo de email"."<br>";
 			if(strlen($_POST['senha']) == 0){
 				echo "Preencha o campo de senha";
 			}
 		}
 		else if(strlen($_POST['senha']) == 0){
 			echo "Preencha o campo de senha"."<br>";
-			if(strlen($_POST['cpf']) == 0){
-				echo "Preencha o campo de CPF";
+			if(strlen($_POST['email']) == 0){
+				echo "Preencha o campo de email";
 			}
 		}
 
 		else{
-			$cpf = $conexao->real_escape_string($_POST['cpf']);
+			$email = $conexao->real_escape_string($_POST['email']);
 			$senha = $conexao->real_escape_string($_POST['senha']);
 			
-			$sql_code = "SELECT * FROM funcionarios WHERE cpf = '$cpf' AND senha = '$senha'";
+			$sql_code = "SELECT * FROM cliente WHERE email = '$email' AND senha = '$senha'";
 			$sql_query = $conexao->query($sql_code) or die("Falha na execução de código SQL ".$conexao->error);
 			
 			$quantidade = $sql_query->num_rows;
@@ -32,13 +32,13 @@
 					session_start();
 				}
 				$_SESSION['id'] = $usuario['id'];
-				$_SESSION['cpf'] = $usuario['cpf'];
+				$_SESSION['email'] = $usuario['email'];
 				
 				header("Location: home.php");
 				
 			}
 			else{
-				echo "Falha ao logar! CPF ou senha incorretos!";
+				echo "Falha ao logar! email ou senha incorretos!";
 			}
 		}
 	}
