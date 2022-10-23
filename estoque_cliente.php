89a688d51b8b792a3197b6df9003669a74bce2fb
Lucas Franco <105955764+Lucasfranken131@users.noreply.github.com>
2022-10-23 10:50:46 -0300

    Update estoque_cliente.php

--- estoque_cliente.php
+++ estoque_cliente.php
@@ -2,26 +2,27 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-	<link rel="stylesheet" href="estilo_itens.css">
+	<link rel="stylesheet" href="estiloitens.css">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movimentação</title>
 </head>
 <body>
     <div>
-		<?php
-			include("conexao.php");
-	    		include("protect_cliente.php");
-		
-			$results_cadastro = 'SELECT * FROM estoque WHERE status = "disponivel"';
-			$resultados_cadastros = mysqli_query($conexao, $results_cadastro);
+	<?php
+		include("conexao.php");	
+		$results_cadastro = 'SELECT * FROM estoque WHERE status = "disponivel"';
+		$resultados_cadastros = mysqli_query($conexao, $results_cadastro);
 
-			while($rows_cadastros = mysqli_fetch_array($resultados_cadastros)){
-				echo '<div class="itens">';
-				echo $rows_cadastros['id']." - ".$rows_cadastros['nome_produto'];
-				echo '</div>';
-			}
-		?>	
-    </div>
+	    	while($rows_cadastros = mysqli_fetch_array($resultados_cadastros)){
+			echo '<form action="pedido.php" method="post">';
+			echo '<div class="conteiner">';
+			echo '<div class="id">'.$rows_cadastros['id'].'</div>'.'<div class="produto">'.'Nome Produto = '.$rows_cadastros['nome_produto'].'</div>'.'<div class="quantidade">'.'Quantidade = '.$rows_cadastros['quantidade'].'</div>';
+			echo '<input type="submit" value="Comprar">';
+			echo '<br>';
+			echo '</div>';
+			echo '</form>';
+		}
+	?>	
 </body>
 </html>
