1c4d93ec7aab79e7b274944c33f3d06bdc35bb00
Lucas Franco <105955764+Lucasfranken131@users.noreply.github.com>
2022-10-23 11:57:38 -0300

    Create enviar.php

---
+++ enviar.php
@@ -0,0 +1,24 @@
+<?php
+include("conexao.php");
+$id= $_POST['id'];
+$nome_produto= $_POST['nome_produto'];
+$sql= "INSERT INTO estoque(id,nome_produto)
+            VALUES('$id','$nome_produto')";
+			   
+        if(mysqli_query($conexao, $sql)){
+
+			$atualizar = "UPDATE estoque SET status = 'disponivel' WHERE id = '$id'"; 
+			if(mysqli_query($conexao, $atualizar)){
+				echo "Produto inserido com sucesso. <br>";
+				echo "Status atualizado com sucesso. <br>";
+				echo "<a href='home_funcionario.php'>Voltar para a Home</a><br>";
+			}	
+			else{
+				echo "ERRO".mysqli_connect_error($conexao);
+			}
+        }
+
+    else{
+        echo "ERRO".mysqli_connect_error($conexao);
+    }
+?>
