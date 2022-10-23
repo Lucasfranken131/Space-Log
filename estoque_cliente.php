1c7637c663ee94eddff981dcd72c7beac18e7dcf
Lucas Franco <105955764+Lucasfranken131@users.noreply.github.com>
2022-10-23 14:50:21 -0300

    Update estoque_cliente.php

--- estoque_cliente.php
+++ estoque_cliente.php
@@ -2,34 +2,33 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-	<link rel="stylesheet" href="estilo_itens.css">
+	<link rel="stylesheet" href="estilocadastro.css">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pedido</title>
+	<link rel="stylesheet" href="estiloitens.css">
+    <title>Movimentação</title>
 </head>
 <body>
     <div>
-		<?php
-			include("conexao.php");
-			include("protect_cliente.php");
-			
-			$usuario = $_SESSION['id_cliente'];
-			$local = "SELECT * FROM cliente WHERE id_cliente = '$usuario'";
-			$local_pull = mysqli_query($conexao, $local);
-			while($local_pull_get = mysqli_fetch_array($local_pull)){
-				echo "Entregar para: ".$local_pull_get['cep']." - ".$local_pull_get['estado']." - ".$local_pull_get['cidade']." - ".$local_pull_get['bairro']." - ".$local_pull_get['rua']." - ".$local_pull_get['numero'];
-				echo "<br><button><a href='estoque_cliente.php'>Confirmar compra</button>";
-			}
-			$id = $_POST['id'];
-			$nome_produto = $_POST=['nome_produto'];
-			
-			$sql = "UPDATE estoque SET status = 'comprado' WHERE id = '$id'"; 
-				if(!mysqli_query($conexao,$sql)){
-					echo "Não foi possível realizar a compra".mysqli_connect_error($conexao);
-				}
-				else{
-				}
-		?>	
-    </div>
+	<center><h1>Selecione o produto para compra</h1>
+	<form action="pedido.php" method="post">
+		<input type="text" placeholder="ID PRODUTO" name="id">
+		<br>
+		<input type="text" placeholder="Nome do produto" name="nome_produto">
+		<br>
+		<input type="submit" value="Comprar">
+	</form></center>
+	<?php
+		include("conexao.php");	
+		$results_cadastro = 'SELECT * FROM estoque WHERE status = "disponivel"';
+		$resultados_cadastros = mysqli_query($conexao, $results_cadastro);
+
+	    	while($rows_cadastros = mysqli_fetch_array($resultados_cadastros)){
+			echo '<div class="conteiner">';
+			echo '<div class="id">'."ID do produto = ".$rows_cadastros['id'].'</div>'.'<div class="produto">'.'Nome Produto = '.$rows_cadastros['nome_produto'].'</div>';
+			echo '<br>';
+			echo '</div>';
+		}
+	?>	
 </body>
 </html>
