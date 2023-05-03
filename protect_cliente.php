965cd105a51c8cabb750601e0184be4185461ce9
Lucas Franco <105955764+Lucasfranken131@users.noreply.github.com>
2023-05-03 00:26:40 -0300

    Delete protect_cliente.php

--- protect_cliente.php
+++
@@ -1,8 +0,0 @@
-<?php
-    if(!isset($_SESSION)){
-        session_start();
-    }
-	if(!isset($_SESSION['id_cliente'])){
-		die("Você não está logado. <p><a href=\"login.html\">Entrar</a></p>");
-	}
-?>
